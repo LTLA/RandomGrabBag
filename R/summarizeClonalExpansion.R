8e6ac2df41304209e8a1bd9cec9368a15f79424d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-19 22:01:43 -0800

    Include Hill numbers in the summarization.

--- R/summarizeClonalExpansion.R
+++ R/summarizeClonalExpansion.R
@@ -7,6 +7,7 @@
 #' @param use.mean Logical scalar indicating whether to report the mean number of cells per clonotype.
 #' @param use.gini Logical scalar indicating whether to report the Gini index.
 #' @param use.top Integer vector specifying the number of clonotypes to use to compute the top percentage.
+#' @param use.hill Integer scalar specifying the orders to use to compute Hill numbers.
 #' 
 #' @return A \linkS4class{DataFrame} with one row per group in \code{counts},
 #' containing summary statistics on the clonal expansion in that group.
@@ -25,6 +26,11 @@
 #' If \code{use.top} is specified, the output will contain the numeric \code{"topX"} columns,
 #' containing the proportion of cells assigned to the top X clonotypes (where X is the value of each \code{use.top} entry).
 #' Larger values indicate that cells are concentrated within a small number of clonotypes.
+#'
+#' If \code{use.hill} is specified, the output will contain numeric \code{"hillX"} columns containing the Hill numbers.
+#' \code{"hill0"} is simply the number of observed clonotypes (i.e., species richness)
+#' while \code{"hill1"} and \code{"hill2"} quantify the evenness of the distribution of cells across clonotypes.
+#' (\code{"hill2"} differs from \code{"hill1"} in that the former gives more weight to dominant clonotypes.)
 #' 
 #' @examples
 #' df <- data.frame(
@@ -40,7 +46,8 @@
 #' 
 #' @export
 #' @importFrom S4Vectors DataFrame
-summarizeClonalExpansion <- function(counts, use.mean=TRUE, use.gini=TRUE, use.top=c(5, 20, 100)) {
+#' @importFrom alakazam calcDiversity
+summarizeClonalExpansion <- function(counts, use.mean=TRUE, use.gini=TRUE, use.top=c(5, 20, 100), use.hill=0:2) {
     stats <- DataFrame(row.names=names(counts))
 
     if (use.mean) {
@@ -54,11 +61,18 @@ summarizeClonalExpansion <- function(counts, use.mean=TRUE, use.gini=TRUE, use.t
             1- sum(cumsum(sort(y))) / (total * (total + 1)/2)
         })
     }
-    
+
     for (i in use.top) {
         stats[[paste0("top", i)]] <- vapply(counts, FUN.VALUE=0, FUN=function(y) {
             sum(head(sort(y, decreasing=TRUE), i))/sum(y) 
         })
+    }
+
+    if (length(use.hill)) {
+        hill <- lapply(counts, calcDiversity, q=use.hill)
+        hill <- do.call(rbind, hill)
+        colnames(hill) <- sprintf("hill%i", use.hill)
+        stats <- cbind(stats, hill)
     }
 
     stats
