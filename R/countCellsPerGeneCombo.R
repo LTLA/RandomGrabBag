da53310ae10788d0b1df0cb4b35f2da2105f3378
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-19 17:45:47 -0800

    Renamed function for clearer purpose, updated docs.

--- R/countCellsPerGeneCombo.R
+++ R/countCellsPerGeneCombo.R
@@ -1,6 +1,6 @@
 #' Count gene combinations
 #'
-#' Count the number of cells that expression each unique combination of genes.
+#' Count the number of cells that express each unique combination of genes.
 #'
 #' @param x A \linkS4class{SplitDataFrameList} where each \linkS4class{DataFrame} is a cell and each row is a sequence.
 #' @param gene.field Character vector of names of columns of \code{x} containing the genes of interest (e.g., VDJ components).
@@ -17,11 +17,13 @@
 #' @details
 #' The aim of this function is to generate a count matrix for use in differential \dQuote{expression} analyses,
 #' i.e., does one particular group of cells express a particular gene combination more frequently than another group? 
+#' This can be useful to examine the effect of particular experimental conditions or the behavior of different cell states,
+#' especially if the specific biological function (e.g., antigen) of each gene combination is known in advance.
 #'
 #' We quote \dQuote{expression} as this is defined in terms of number of cells expressing the gene,
 #' rather than the more typical quantity of the number of reads or UMIs assigned to that gene.
 #' Note that this raises some difficult questions about normalization when the sequencing coverage varies between groups;
-#' we assume that such changes affect the probability of detecting all gene combinations equally,
+#' we assume that such changes have the same scaling effect on the probability of detecting each gene combination,
 #' such that it cancels out after normalizing by the total number of cells.
 #'
 #' If \code{cov.field} is set, only the most high-abundance sequence is used from each cell.
@@ -36,11 +38,11 @@
 #' )
 #'
 #' y <- splitToCells(df, field="cell.id")
-#' out <- countGeneCombos(y, c("v_gene", "j_gene"))
+#' out <- countCellsPerGeneCombo(y, c("v_gene", "j_gene"))
 #' rowData(out)
 #' assay(out)
 #'
-#' out2 <- countGeneCombos(y, c("v_gene", "j_gene"), 
+#' out2 <- countCellsPerGeneCombo(y, c("v_gene", "j_gene"), 
 #'    group=sample(10, length(y), replace=TRUE))
 #' rowData(out2)
 #' assay(out2)
@@ -48,7 +50,7 @@
 #' @export
 #' @importFrom S4Vectors selfmatch
 #' @importFrom IRanges IntegerList
-countGeneCombos <- function(x, gene.field, group=NULL, cov.field=NULL) {
+countCellsPerGeneCombo <- function(x, gene.field, group=NULL, cov.field=NULL) {
     if (!is.null(cov.field)) {
         cov <- x[,cov.field]
         x <- x[cov==max(cov)]
